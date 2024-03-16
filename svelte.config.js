53ebe42cb1114a696e3bb49b6c67daf6c7bd8d72
Anders Ekdahl <anders@commercemind.se>
2024-03-16 10:06:23 +0100

    Add CSP config

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,17 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		csp: {
+			mode: "auto",
+			directives: {
+			    "default-src": ["'self'"],
+			    "base-uri": ["'self'"],
+			    "connect-src": ["'self'"],
+			    "img-src": ["'self'", "data:"],
+			    "frame-src": ["'self'"],
+			},
+		},
+
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
